862cad8ab71b832022d4b50076c462c2dc9b7640
magi-Z <63131352+magi-Z@users.noreply.github.com>
2025-01-02 23:18:22 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -61,8 +61,8 @@ PACKAGES="$PACKAGES luci-i18n-udpxy-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-wechatpush-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-wifischedule-zh-cn"
-#PACKAGES="$PACKAGES luci-i18n-socat-zh-cn"
-#PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-socat-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
