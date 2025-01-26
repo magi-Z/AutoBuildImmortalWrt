c36b740fbf00759ebaa5000b825dfc554f70158d
magi-Z <63131352+magi-Z@users.noreply.github.com>
2025-01-26 23:25:38 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -72,23 +72,16 @@ PACKAGES="$PACKAGES luci-i18n-cifs-mount-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-wechatpush-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-adblock-fast-zh-cn"
-#
 PACKAGES="$PACKAGES luci-i18n-wifischedule-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-base-zh-cn"
 
 ##
-PACKAGES="$PACKAGES luci-i18n-socat-zh-cn"
-
-
 
 #PACKAGES="$PACKAGES luci-i18n-ahcp-zh-cn"
 #PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
 #PACKAGES="$PACKAGES luci-i18n-udpxy-zh-cn"
-#
-#
-#
-#
+#PACKAGES="$PACKAGES luci-i18n-socat-zh-cn"
 #PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
 #PACKAGES="$PACKAGES luci-i18n-adbyby-plus-zh-cn"
 #PACKAGES="$PACKAGES luci-i18n-mwan3-zh-cn"
